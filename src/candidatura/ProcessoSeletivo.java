d4bb2247e110e2a0dde70def5082b554faf7ac7b
clivissonjose <cjbarbosa2001@gmail.com>
2024-05-13 19:37:53 -0300

    analisando candidatos

---
+++ src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,28 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		analisarCandidato(1890);
+		analisarCandidato(2000);
+		analisarCandidato(3000);
+
+	}
+	
+	static void analisarCandidato(double salarioPretendido) {
+		
+		
+		double salarioBase = 2000.00;
+		
+		if(salarioBase > salarioPretendido) {
+			System.out.println("LIGAR PARA O CANDIDATO");
+		}else if( salarioBase ==  salarioPretendido) {
+			System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA!");
+		}else {
+			System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS!");
+		}
+	}
+
+}
