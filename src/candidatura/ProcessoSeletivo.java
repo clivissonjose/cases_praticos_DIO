8df7d09fa4aac6cec4722cb16e7142b72df33251
clivissonjose <cjbarbosa2001@gmail.com>
2024-05-13 23:06:37 -0300

    feat: selecionando candidatos aprovados

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,18 +1,51 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		analisarCandidato(1890);
+	/*	analisarCandidato(1890);
 		analisarCandidato(2000);
-		analisarCandidato(3000);
+		analisarCandidato(3000);  */
+		
+		selecaoCandidatos();
+		
 
 	}
 	
-	static void analisarCandidato(double salarioPretendido) {
+	static void selecaoCandidatos() {
 		
+		String [] candidatos = {"Clívisson", "Rafaela", "Nayara", "Kaik", "Jackson", "Alice", "Augusto", "Clivia"};
+		
+		double salarioBase =  2000;
+		int candidatosSelecionados = 0;
+		int indice = 0;
+		
+		while(candidatosSelecionados < 5 && indice < candidatos.length) {
+			String candidato = candidatos[indice];
+			
+			double valor = valorPretendido();
+			
+			if(salarioBase >= valor) {
+				System.out.println("O(a) candidato(a) " +candidato +" esta aprovado com salario " + valor);
+				candidatosSelecionados++;
+			}else {
+				System.out.println("O(a) candidato " +candidato +" não foi aprovado! queria um salario de " + valor);
+			}
+	       indice++;
+			
+		}
+		
+	}
+	
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800,2200); 
+	}
+	
+	static void analisarCandidato(double salarioPretendido) {
 		
 		double salarioBase = 2000.00;
 		
